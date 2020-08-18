849b1deb534077d35691937cac06e4742611ee8e
Joshua Mclean <joshmcleana@gmail.com>
2020-08-18 15:16:31 -0700

    Update Controllers with proper validation order

--- Gemfile
+++ Gemfile
@@ -46,3 +46,5 @@ gem 'jsonapi-serializer'
 
 gem "faker", "~> 2.13"
 gem "figaro", "~> 1.2"
+gem "rspec"
+gem ""
--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,16 +1,16 @@
 class Api::V1::AuthController < ApplicationController
-    skip_before_action :authorized, only: [:create]
-    
-    def create
-        user = User.find_by(user_name: user_login_params[:username])
-        #User#authenticate from bcrypt checking the pass against the salted
-        if user && user.authenticate(user_login_params[:password])
-            token = encode_token({user_id: user.id})
-            render json: {id: user.id, fullname: user.full_name, username: user.user_name, jwt: token }, status: :accepted
-        else
-            render json: {message: 'Invalid login credentials.'}, status: :unauthorized
-        end
+  skip_before_action :authorized, only: [:create]
+  
+  def create
+    user = User.find_by(user_name: user_login_params[:username])
+    #User#authenticate from bcrypt checking the pass against the salted
+    if user && user.authenticate(user_login_params[:password])
+        token = encode_token({user_id: user.id})
+        render json: {id: user.id, fullname: user.full_name, username: user.user_name, jwt: token }, status: :accepted
+    else
+        render json: {message: 'Invalid login credentials.'}, status: :unauthorized
     end
+  end
 
     def show
         # byebug
