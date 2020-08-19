55d977149e7546708fe785cdfed173e3429a931b
Joshua Mclean <joshmcleana@gmail.com>
2020-08-18 17:23:34 -0700

    Updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,6 +7,14 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
+    origins 'https://carecast.herokuapp.com/'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+  
+  allow do
     origins 'http://localhost:3001'
 
     resource '*',
@@ -14,11 +22,4 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 
-  allow do
-    origins 'https://carecast.herokuapp.com/'
-
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
 end
