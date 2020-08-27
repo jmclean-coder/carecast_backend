05c71102093406bbd2dd2b20c9ad3e34976ec973
Joshua Mclean <joshmcleana@gmail.com>
2020-08-26 20:07:50 -0700

    Edited byebug.rb

--- config/initializers/byebug.rb
+++ config/initializers/byebug.rb
@@ -1,3 +1,4 @@
 if Rails.env.production?
-    Byebug.start_server 'localhost', ENV.fetch("BYEBUG_SERVER_PORT", 1048).to_i
-  end
+    require 'byebug/core'
+  Byebug.start_server 'localhost', ENV.fetch("BYEBUG_SERVER_PORT", 1048).to_i
+end
